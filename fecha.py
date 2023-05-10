1083f5470fe7ec36087ec5c2cc430a8683b3bfc7
Augustoiacopini <adiacopini@gmail.com>
2023-05-10 13:27:18 -0300

    Update fecha.py

--- fecha.py
+++ fecha.py
@@ -10,7 +10,7 @@ class Fecha:
             
         querystring = {"league":"140","season":2022,"date":self.fecha}
 
-        headers = {"x-rapidapi-key": "2ede2642bbmshcf1ffab0810726cp198229jsn6890c02ecc02",
+        headers = {"x-rapidapi-key": "*******************************",
                     "x-rapidapi-host": "api-football-beta.p.rapidapi.com"}
 
         response = requests.request("GET", url, headers=headers, params=querystring)
