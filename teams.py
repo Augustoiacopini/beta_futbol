d469bea5e7bb62bc7074fa2fc7383220e372397b
Augustoiacopini <adiacopini@gmail.com>
2023-05-10 13:29:42 -0300

    teams.py

--- teams.py
+++ teams.py
@@ -1,7 +1,7 @@
 import requests
 """ devuelve todos los equipos de la liga española"""
 url = f"https://api-football-beta.p.rapidapi.com/teams?"
-headers = {"x-rapidapi-key": "2ede2642bbmshcf1ffab0810726cp198229jsn6890c02ecc02",
+headers = {"x-rapidapi-key": "*********************************",
                     "x-rapidapi-host": "api-football-beta.p.rapidapi.com"}
 
 querystring = {"league":140, "season":2022}
